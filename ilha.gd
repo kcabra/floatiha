4fc37ecda649224e6386e39a21a26eb71dadfcba
kalil <kcabrak@gmail.com>
2019-08-21 20:23:36 -0300

    before the (shit?) storm

--- ilha.gd
+++ ilha.gd
@@ -2,11 +2,16 @@ extends Area2D
 
 onready var tween = $Tween
 onready var sprite = $Sprite
+
 enum cores {verde, amarelo, cristal}
 export (cores) var tipo_atual = cores.verde
 enum tamanhos {pequena, grande}
 export (tamanhos) var tamanho_atual = tamanhos.pequena
+
 export (float, 0, 2, 0.1) var DURATION = 0.2
+export (float, 0, 300, 0.1) var POSITION
+
+var up_range
 
 func _ready():
 	var cor
@@ -20,17 +25,16 @@ func _ready():
 		tamanhos.pequena:
 			tamanho = "pequena"
 	sprite.animation = cor + "-" + tamanho
+	up_range = -130
 	var count = sprite.frames.get_frame_count(sprite.animation)
 	randomize()
 	sprite.frame = randi() % count
-	match tamanho_atual:
-		tamanhos.grande:
-			pass
 	rotation_degrees = rand_range(45, -45)
 
 func raise():
 	randomize()
-	tween.interpolate_property(self, "position:y", position.y, position.y + rand_range(-32, -64),
+	var baseline = -30
+	tween.interpolate_property(self, "position:y", position.y, position.y + rand_range(baseline, baseline + up_range),
 			DURATION, Tween.TRANS_EXPO, Tween.EASE_OUT)
 	tween.interpolate_property(self, "rotation_degrees", rotation_degrees, 0,
 			DURATION, Tween.TRANS_EXPO, Tween.EASE_OUT)

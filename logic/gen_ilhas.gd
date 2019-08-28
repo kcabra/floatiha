d81e138b96e763566a9e91c3aacb35da880cf226
llk <llkcbr@gmail.com>
2019-08-28 19:49:27 -0300

    midseason break go home

--- logic/gen_ilhas.gd
+++ logic/gen_ilhas.gd
@@ -2,17 +2,18 @@ extends EditorScript
 
 # each island is a dict on a array
 # each dict contains the following info:
-# mus_position: a posição da ilha na música
-# duration: quanto tempo leva pra ilha subir
+# type: qual o tipo da ilha (verde, amarela, cristal)
 # size: se a ilha é do tipo pequena ou grande
 # (opt) sprite: qual o indice do sprite da ilha
+# mus_position: a posição da ilha na música
+# duration: quanto tempo leva pra ilha subir
 # height: o quanto a ilha sobe
 # (opt) init_y: variação em relação ao chao
 # (opt) init_rot: rotação inicial
 
 var island_scene
 var island_root
-var island_array_times = Array()
+var island_array_times
 var island_array_nodes
 
 func _run():
@@ -34,6 +35,9 @@ func create_islands():
 		else: ilha.init_rot = rand_range(-45, 45)
 		if entry.has("duration"): ilha.duration = entry.duration
 		else: ilha.duration = 0.2
+		var sprite_number = entry.sprite if entry.has("sprite") else randi() % 
+		sprite_name = "%s-%s%d.png" % [entry.type, entry.size, sprite_number]
+		ilha.get_node("sprite").texture = load("res://assets/ilhas/"
 		
 		ilha.add_child(island_root)
 		ilha.add_owner(get_scene())
